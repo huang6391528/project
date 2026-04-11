59858483bde5dc31ea133071ab5a347670d3ce1c
Hairui Huang <huang6391528@gmail.com>
2026-04-12 07:08:14 +0800

    fix: 修复vite base配置导致的相对路径崩溃问题

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 
 export default defineConfig({
-  base: './',
+  base: '/', // 👈 必须改成绝对路径 '/'，千万不能带点！
 
   plugins: [uni()],
 
   build: {
     outDir: 'dist/build/h5',
     assetsDir: 'assets',
+    // 下面的 rollupOptions 保持你的原样即可
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name]-[hash][extname]',
