082055d1bc43285747e308fa83ac0dbea77b96c6
Hairui Huang <huang6391528@gmail.com>
2026-04-12 06:41:10 +0800

    fix: disable preload

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,10 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 
 export default defineConfig({
-  base: '/',
-
+  base: './',
   plugins: [uni()],
-
   build: {
     outDir: 'dist/build/h5',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
+    modulePreload: false   // ⭐ 关键
   }
 })
